9c28580c6e1ce90840c7e420e0441e7d78b6d352
lynn2610 <lynn_mg_study@163.com>
2018-12-25 16:49:59 +0800

    实现了画矩形的流程

--- callTangramUI.py
+++ callTangramUI.py
@@ -17,20 +17,23 @@ class MyMainWidget(QWidget, Ui_tangramForm):
         super(MyMainWidget, self).__init__(parent)
         self.setupUi(self)
 
-        # self.imgcboBox.addItem('1.jpg')
-        # self.imgcboBox.addItem('4.jpg')
+        # 下拉列表添加图片信息
         self.imgcboBox.addItems(
             [
                 '1.jpg', '4.jpg', '5.jpg', 'b1.jpg', 'b2.jpg',  'cat01.jpg',
                 'animal01.jpg', 'people01.jpg', 'people02.jpg'
              ]
         )
-        self.imgcboBox.currentIndexChanged.connect(self.selectionchange)
+        self.imgcboBox.currentIndexChanged.connect(self.selectionchange) # 下拉列表添加事件处理
+        # Radio Button 添加事件处理
         self.sourceImageRBtn.toggled.connect(lambda : self.btnstate(self.sourceImageRBtn))
         self.personalDesignRBtn.toggled.connect(lambda : self.btnstate(self.personalDesignRBtn))
         # 默认显示 图像实例中的选中图像
         self._imgPix = QPixmap('./mould/' + self.imgcboBox.currentText())
         self.imgLable.setPixmap(self._imgPix)
+
+        self.lastPoint = QPoint()
+        self.endPoint = QPoint()
 
 
     def btnstate(self, btn):
@@ -47,15 +50,12 @@ class MyMainWidget(QWidget, Ui_tangramForm):
             self.imgLable.setPixmap(self._imgPix)
             # self.imgcboBox.setVisible(True)
         else:
+            # 切换到个人设计模块时，进行初始化
             self.imgcboBox.setEnabled(False)
             self.imgLable.clear()
-            # self._imgPix = QPixmap('./mould/painting.jpg')
-            # self.imgLable.setPixmap(self._imgPix)
-            self._imgPix = QPixmap()
-            self.lastPoint = QPoint()
-            self.endPoint = QPoint()
             self.initImageLable()
-            self.imgLable.setPixmap(self._imgPix)
+
+
 
     def initImageLable(self):
         '''
@@ -63,8 +63,54 @@ class MyMainWidget(QWidget, Ui_tangramForm):
         :return:
         '''
         # 设置大小为 900 * 600
+        self._imgPix = QPixmap()
         self._imgPix = QPixmap(900, 600)
         self._imgPix.fill(Qt.white)
+        self.imgLable.setPixmap(self._imgPix)
+
+        self.lastPoint = QPoint()
+        self.endPoint = QPoint()
+
+
+    def paintEvent(self, event):
+        '''
+        绘画事件。
+        :param event:
+        :return:
+        '''
+        if self.personalDesignRBtn.isChecked() == False:
+            return;
+
+        x = self.lastPoint.x() - self.imgLable.x()
+        y = self.lastPoint.y() - self.imgLable.y()
+        w = self.endPoint.x() - self.imgLable.x() - x
+        h = self.endPoint.y() - self.imgLable.y() - y
+
+        pp = QPainter(self._imgPix)
+        print(x, y, w, h)
+        # pp.setBrush(QColor(0, 0, 0))
+        pp.drawRect(x, y, w, h)
+
+        # painter.drawPixmap(0, 0, self._imgPix)
+        self.imgLable.setPixmap(self._imgPix)
+
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.lastPoint = event.pos()
+            self.endPoint = self.lastPoint
+
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() and Qt.LeftButton:
+            self.endPoint = event.pos()
+            self.update()
+
+
+    def mouseReleseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.endPoint = event.pos()
+            self.update()
 
 
     def selectionchange(self,i):
